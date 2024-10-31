0b31dd7d6a30a310bfc87c7b017ca09e9cdb1b96
paulinasprojects <virtualpaula93@gmail.com>
2024-10-31 11:47:31 +0200

    added responsivness for laptop, tablet and phone screens

--- src/components/search-hotel-card.tsx
+++ src/components/search-hotel-card.tsx
@@ -9,11 +9,11 @@ const SearchHotelCard = () => {
   return (
     <div className="flex flex-col gap-10 ">
       {searchHotelsData.map((data) => (
-      <div key={data.id} className="border-[1px] border-slate-200 rounded-[20px] pr-10">
+      <div key={data.id} className="rounded-[20px] xl:pr-10 md:pr-0 max-sm:pr-0 border-[1px] border-slate-200 max-sm:m-5 md:m-2 lg:m-0">
         <div className="flex gap-[30px] xl:flex-row max-sm:flex-col lg:flex-col">
-          <img src={data.image} alt="" />
-          <div className="flex flex-col  pt-10 ">
-          <h3 className="text-headlineTwo font-bold font-dm-sans">{data.name}</h3>
+          <img src={data.image} alt="" className="lg:rounded-[20px] xl:rounded-none max-sm:rounded-[20px]" />
+          <div className="flex flex-col  xl:pt-10 md:pt-5 lg:pt-0 max-sm:ml-5 lg:m-5 xl:m-0">
+          <h3 className="text-headlineTwo font-bold font-dm-sans max-sm:text-headlineThree">{data.name}</h3>
           <div className="mt-5 flex gap-2 items-center">
             <FaStar className="text-yellow-400"/>
             <span className="text-sm font-medium font-dm-sans">{data.rating}</span>
@@ -23,7 +23,7 @@ const SearchHotelCard = () => {
               {data.location}
             </span>
           </div>
-          <div className="flex gap-5 mt-10">
+          <div className="flex gap-5 mt-10 max-sm:flex-col">
             <span className="flex items-center gap-2 text-md font-dm-sans font-normal">
               <MapPin className="text-[#84878B] size-6"/>
               {data.hotelName}
@@ -39,7 +39,7 @@ const SearchHotelCard = () => {
               Departure from {data.departure}
             </span>
           </div> 
-          <div className="flex items-center justify-between mt-[41px]">
+          <div className="flex items-center justify-between mt-[41px] md:gap-5 lg:gap-0 max-sm:flex-col max-sm:items-start">
             <div className="flex flex-col gap-1.5">
             {accomodationsData.map((data, index) => (
               <div key={index} className="">
@@ -50,12 +50,12 @@ const SearchHotelCard = () => {
               </div>
             ))}
             </div>
-            <div className="flex flex-col gap-5 p-5">
+            <div className="flex flex-col gap-5 p-5 max-sm:p-0 max-sm:pl-2 max-sm:mt-5 max-sm:mb-5">
               <div className="flex items-center gap-1.5">
-                <span className="text-[28px] font-bold font-dm-sans">${data.price}</span> 
+                <span className="text-[28px] lg:text-[28px] font-bold font-dm-sans max-sm:text-lg md:text-lg">${data.price}</span> 
                 <span className="text-md font-dm-sans text-[#84878B]">For Two</span>
               </div>
-              <button className="bg-blue100 text-white rounded-[30px] px-7 py-3 text-xl font-medium font-dm-sans">Book now</button>
+              <button className="bg-blue100 text-white rounded-[30px] lg:px-7 lg:py-3 lg:text-xl font-medium font-dm-sans max-sm:px-5 max-sm:py-2 md:px-5 md:py-2 md:text-md max-sm:text-sm">Book now</button>
             </div>
           </div>
           </div>
