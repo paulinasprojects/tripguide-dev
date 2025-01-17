a7b0d144719a0e022ceaf8cf06323c1af6d57d84
paulinasprojects <virtualpaula93@gmail.com>
2025-01-17 14:32:14 +0200

    added some padding

--- src/components/top-tour-card.tsx
+++ src/components/top-tour-card.tsx
@@ -1,6 +1,6 @@
 const TopTourCard = () => {
   return (
-    <div className='flex md:flex-row xl:gap-[30px] lg:gap-5 md:gap-5 items-center justify-center mt-[50px] max-sm:flex-col max-sm:gap-5'>
+    <div className='flex md:flex-row xl:gap-[30px] lg:gap-5 md:gap-5 items-center justify-center mt-[50px] max-sm:flex-col max-sm:gap-5 md:px-5 lg:px-0'>
       <div className="relative">
         <img src="https://res.cloudinary.com/dymlzmyuo/image/upload/v1729963616/tokyo_so1nui.jpg" alt="" className="xl:w-[370px] md:w-[300px] xl:h-[495px] md:h-[408px] max-sm:h-[495px] max-sm:w-[335px] rounded-[20px] object-cover cursor-pointer" />
         <span className="absolute top-5 left-5 px-6 py-2 bg-[#141416]/40 text-white100 rounded-[20px] text-xl font-medium font-dm-sans">Japan</span>
