d1fb639826b2b3c5b63c8b2cf2537b10f34f239c
paulinasprojects <virtualpaula93@gmail.com>
2025-01-17 14:28:03 +0200

    changed the width of some images

--- src/components/trending-cities-card.tsx
+++ src/components/trending-cities-card.tsx
@@ -5,7 +5,7 @@ const TrendingCitiesCard = () => {
   return (
     <>
       {trendingCitiesData.map((city) => (
-        <div key={city.id} className="bg-white rounded-[16px] lg:w-[450px] md:w-[350px] md:h-[215px] max-sm:w-[370px] max-sm:h-[221px] cursor-pointer">
+        <div key={city.id} className="bg-white rounded-[16px] xl:w-[400px] lg:w-[450px] md:w-[350px] md:h-[215px] max-sm:w-[370px] max-sm:h-[221px] cursor-pointer">
           <div className="flex  gap-[30px] mt-[23px] ml-[24px]">
             <img src={city.image} alt="" className="w-[160px] h-[168px] rounded-[20px] object-cover" />
             <div className="flex flex-col gap-2.5">
